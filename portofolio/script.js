8de2b065c1a21c0e01441a62f7a79cf0909d5fbd
MohSolehuddin <moh.solehuddin190805@gmail.com>
2023-10-29 07:22:29 +0700

    perbaikan tampilan web portofolio

--- portofolio/script.js
+++ portofolio/script.js
@@ -67,15 +67,125 @@ function TampilkanSertifikat() {
   prints("JavaScript","sertifikat/js/js.webp", "sertifikat/js/js2.webp", "sertifikat/js/js3.webp")
   prints("aws","sertifikat/aws/aws.webp", "sertifikat/aws/aws2.webp", "sertifikat/aws/aws3.webp")
 }
-function waktu(){
+function exp(){
   let from = new Date("19 September 2021");
   let to = new Date();
   if (from.getYear()<=to.getYear()){
     hasilTahun= to.getYear() - from.getYear();
     hasilTahun *=12;
     hasilBulan = to.getMonth()+1;
-    hasil = hasilTahun-9 + hasilBulan;
-    document.getElementById("Waktu").innerHTML = ` kurang lebih ${hasil} Bulan`;
+    hasil = Math.floor((hasilTahun-9 + hasilBulan)/12);
+    document.getElementById("exp").innerHTML = `${hasil}+ year`;
   }
 }
-setInterval(function() {}, 10);
+const portofolio = [
+  {
+    title: "Number Convertion",
+    img: "",
+    link: "https://konversi-angka.vercel.app"
+  }
+];
+
+function printPortofolio() {
+  let result= "Ser";
+  let output = document.getElementById("output");
+  for (let i in portofolio) {
+    result+= `
+    <h3>${portofolio[i].title}</h3>
+    
+    `
+  }
+  output.innerHTML = result;
+}
+
+function printSkill() {
+  let output = document.getElementById("output");
+  output.innerHTML =
+  `
+  <!-- Skill -->
+    <section id="Skill" style="height: 60px;"></section>
+    <section class="skill text-center mb-5" data-aos="fade-right">
+      <div class="card">
+        <div class="body-card">
+          <h3 class="text-primary" style="font-family: 'Kdam Thmor Pro', sans-serif;">Sertifikat Skill</h3>
+          <select name="OptionKompetensi" id="OptionKompetensi" onclick="TampilkanSertifikat()">
+            <option value="HTML" onclick="TampilkanSertifikat()">HTML</option>
+            <option value="CSS" onclick="TampilkanSertifikat()">CSS</option>
+            <option value="JavaScript" onclick="TampilkanSertifikat()">JavaScript</option>
+            <option value="Python" onclick="TampilkanSertifikat()">Python</option>
+            <option value="BE" onclick="TampilkanSertifikat()">Back End(nodejs)</option>
+            <option value="aws" onclick="TampilkanSertifikat()">Dasar aws</option>
+          </select>
+          <button class="btn-primary" type="submit" onclick="TampilkanSertifikat()">Lihat</button>
+        <div id="OutputSertifikat" class="mt-4 goncangan"></div>
+        <script type="text/javascript" charset="utf-8">
+          TampilkanSertifikat();
+        </script>
+      </div>
+    </div>
+  </section>
+  `;
+}
+const product = [
+  {
+    img: "",
+    title: "Paket Silver",
+    keterangan: "silver"
+  },
+  {
+    img: "",
+    title: "Paket Gold",
+    keterangan: "Gold"
+  },
+  {
+    img: "",
+    title: "Paket Premium",
+    keterangan: "Premium"
+  },
+  {
+    img: "",
+    title: "Paket Diamond",
+    keterangan: "Diamond"
+  },
+  ]
+function printProduct() {
+  let result = `<section id="my-product" class="container">
+    <h1 class="text-primary text-center" style="font-family: 'Kdam Thmor Pro', sans-serif;">PRODUK</h1><div class="glide">
+  <div class="glide__track" data-glide-el="track">
+    <ul class="glide__slides">`;
+  let output = document.getElementById("output");
+  for (let i in product) {
+    result+= `
+    <div class="row mb-5 mt-5 justify-content-evenly text-center">
+      <div class="col-sm-6">
+        <div class=" my-card mt-5 mb-5" data-aos="fade-up">
+          <img src="${product[i].img}property/solehuddin.png" class="card-img-top" alt="Portofolio">
+          <div class="card-body">
+            <h5 class="card-title">${product[i].title}</h5>
+            <p class="card-text">
+              ${product[i].keterangan}
+            </p>
+            <a href="#" onclick="OrderNow()" class="btn btn-primary">Order Now</a>
+          </div>
+        </div>
+      </div>
+      <hr/>
+    
+    `
+  }
+  output.innerHTML = `${result}</section></ul>
+  </div>
+</div>`;
+}
+function animationScrollReveal(delayValue, array) {
+  const scrollAnimation = ScrollReveal(
+    {origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: delayValue,
+  });
+  for (let i in array) {
+    delayValue += 100;
+    scrollAnimation.reveal(`${array[i]}`, {delay: delayValue});
+  }
+}
